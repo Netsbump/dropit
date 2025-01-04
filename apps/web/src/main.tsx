afdb76d28b5282b00035d4aaac97bee9f5fe696b
Sten Levasseur <levasseur.sten@gmail.com>
2025-01-04 17:35:11 +0100

    chore: setup tanstack router

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,12 +1,26 @@
+import { RouterProvider, createRouter } from '@tanstack/react-router';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+// Import the generated route tree
+import { routeTree } from './routeTree.gen';
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Root element not found');
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <div>Hello from Web</div>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
