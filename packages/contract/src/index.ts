065e4017d2837fe75f2f3bd8a92086dce1d5a6ab
Sten Levasseur <levasseur.sten@gmail.com>
2025-01-18 23:21:27 +0100

    ✨ feat(packages): add ComplexCategory schema and contract

--- packages/contract/src/index.ts
+++ packages/contract/src/index.ts
@@ -1,4 +1,5 @@
 import { initContract } from '@ts-rest/core';
+import { complexCategoryContract } from './complex-category.contract';
 import { complexContract } from './complex.contract';
 import { exerciseCategoryContract } from './exercise-category.contract';
 import { exerciseContract } from './exercise.contract';
@@ -9,8 +10,10 @@ export const apiContract = c.router({
   exercise: exerciseContract,
   exerciseCategory: exerciseCategoryContract,
   complex: complexContract,
+  complexCategory: complexCategoryContract,
 });
 
 export * from './exercise.contract';
 export * from './exercise-category.contract';
 export * from './complex.contract';
+export * from './complex-category.contract';
